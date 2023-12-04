b3b4e8bfc6e80676e325ac70a21c2580a3cbcd57
Morfin Overtkill <sanfir@yandex.ru>
2023-12-04 18:38:55 +0300

    dec 4

--- src/Model/winograd.cc
+++ src/Model/winograd.cc
@@ -1,41 +1,65 @@
 #include "winograd.h"
 
-int main() {
-  std::vector<std::vector<double>> one{{1, 2, 3, 4, 5},
-                                       {4, 5, 6, 4, 5},
-                                       {7, 8, 9, 4, 5},
-                                       {4, 5, 4, 4, 5},
-                                       {4, 5, 4, 5, 5}};
-  std::vector<std::vector<double>> two{{1, 2, 3, 4, 5},
-                                       {4, 5, 6, 4, 5},
-                                       {7, 8, 9, 4, 5},
-                                       {4, 5, 4, 4, 5},
-                                       {4, 5, 4, 5, 5}};
-  // SEQUENCE ALGO!
-  //   Winograd w(one, two);
-  //   auto row_f = w.RowFactor();
-  //   auto col_f = w.ColFactor();
-  //   auto res = w.WinogradMulti(row_f, col_f);
+void Load(std::vector<std::vector<double>> &matrix, const std::string &path) {
+  std::ifstream in(path);
+  std::string temp;
+  int x;
+  int c = 0;
 
-  //   if (one.size() % 2 != 0) w.IfNotEven(res);
-
-  //   for (auto &r : res) {
-  //     for (auto &print : r) {
-  //       std::cout << print << " ";
-  //     }
-  //     std::cout << std::endl;
-  //   }
-
-  Winograd w(one, two);
-  auto res = w.Pipeline();
-
-  if (one.size() % 2 != 0) w.IfNotEven(res);
-
-  for (auto &r : res) {
-    for (auto &print : r) {
-      std::cout << print << " ";
+  while (getline(in, temp)) {
+    std::istringstream ss(temp);
+    while (ss >> x) {
+      matrix[c].emplace_back(x);
     }
-    std::cout << std::endl;
+    c++;
   }
+}
+
+int main() {
+  std::vector<std::vector<double>> one;
+  one.resize(500);
+  std::vector<std::vector<double>> two;
+  two.resize(500);
+  std::string file = "/Users/morfinov/GHPara/src/Model/file_500.txt";
+  std::string file_2 = "/Users/morfinov/GHPara/src/Model/file_500.txt";
+  Load(one, file);
+  Load(two, file_2);
+  std::cout << "LOAD DONE" << std::endl;
+  // SEQUENCE ALGO!
+  Winograd st(one, two);
+  int c = 0;
+  auto t1 = std::chrono::high_resolution_clock::now();
+  while (c++ < 100) {
+    auto res = st.Standart();
+  }
+  auto t2 = std::chrono::high_resolution_clock::now();
+  auto total_1 =
+      std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count();
+  std::cout << "seq time - " << total_1 << std::endl;
+
+  // for (auto &r : res) {
+  //   for (auto &print : r) {
+  //     std::cout << print << " ";
+  //   }
+  //   std::cout << std::endl;
+  // }
+
+  Winograd pipe(one, two);
+  int t = 0;
+  auto t3 = std::chrono::high_resolution_clock::now();
+  while (t++ < 100) {
+    auto res_pipe = pipe.Pipeline();
+  }
+  auto t4 = std::chrono::high_resolution_clock::now();
+  auto total_2 =
+      std::chrono::duration_cast<std::chrono::seconds>(t4 - t3).count();
+  std::cout << "pipeline time - " << total_2 << std::endl;
+
+  // for (auto &r : res_pipe) {
+  //   for (auto &print : r) {
+  //     std::cout << print << " ";
+  //   }
+  //   std::cout << std::endl;
+  // }
   return 0;
 }
--- src/Model/winograd.h
+++ src/Model/winograd.h
@@ -3,8 +3,14 @@
 
 #include <omp.h>
 
+#include <chrono>
+#include <condition_variable>
+#include <fstream>
 #include <iostream>
+#include <mutex>
+#include <ostream>
 #include <queue>
+#include <sstream>
 #include <thread>
 #include <vector>
 
@@ -13,14 +19,29 @@ class Winograd {
   Winograd(std::vector<std::vector<double>>& in_1,
            std::vector<std::vector<double>>& in_2)
       : in_1_(in_1), in_2_(in_2) {
-    row_f_size_ = in_1_.size();
-    col_f_size_ = in_2_[0].size();
-    half_ = row_f_size_ / 2;
+    row_one = in_1_.size();
+    col_one = in_1_[0].size();
+    row_two = in_2_.size();
+    col_two = in_2_[0].size();
+    if (row_two % 2 == 0) even_ = true;
+    half_ = row_two / 2;
+  }
+
+  std::vector<std::vector<double>> Standart() {
+    auto row_f = RowFactor();
+
+    auto col_f = ColFactor();
+
+    auto res = WinogradMulti(row_f, col_f);
+
+    if (!even_) IfNotEven(res);
+
+    return res;
   }
 
   std::vector<double> RowFactor() {
-    std::vector<double> result(row_f_size_);
-    for (int row = 0; row < row_f_size_; ++row) {
+    std::vector<double> result(row_one);
+    for (int row = 0; row < row_one; ++row) {
       for (int j = 0; j < half_; ++j)
         result[row] += in_1_[row][2 * j] * in_1_[row][2 * j + 1];
     }
@@ -28,8 +49,18 @@ class Winograd {
   }
 
   std::vector<double> ColFactor() {
-    std::vector<double> result(col_f_size_);
-    for (int col = 0; col < col_f_size_; ++col) {
+    std::vector<double> result(col_two);
+    for (int col = 0; col < col_two; ++col) {
+      for (int j = 0; j < half_; ++j)
+        result[col] += in_2_[2 * j][col] * in_2_[2 * j + 1][col];
+    }
+    return result;
+  }
+
+  std::vector<double> ColFactorParallel() {
+    std::vector<double> result(col_two);
+#pragma omp parallel for schedule(dynamic)
+    for (int col = 0; col < col_two; ++col) {
       for (int j = 0; j < half_; ++j)
         result[col] += in_2_[2 * j][col] * in_2_[2 * j + 1][col];
     }
@@ -38,11 +69,11 @@ class Winograd {
 
   std::vector<std::vector<double>> WinogradMulti(std::vector<double>& row_f,
                                                  std::vector<double>& col_f) {
-    std::vector<std::vector<double>> result(row_f_size_);
-    for (auto& row : result) row.resize(col_f_size_);
+    std::vector<std::vector<double>> result(row_one);
+    for (auto& row : result) row.resize(col_two);
 
-    for (int row = 0; row < row_f_size_; ++row) {
-      for (int col = 0; col < col_f_size_; ++col) {
+    for (int row = 0; row < row_one; ++row) {
+      for (int col = 0; col < col_two; ++col) {
         result[row][col] = -row_f[row] - col_f[col];
         for (auto k = 0; k < half_; ++k) {
           result[row][col] += (in_1_[row][2 * k] + in_2_[2 * k + 1][col]) *
@@ -55,84 +86,224 @@ class Winograd {
   }
 
   void IfNotEven(std::vector<std::vector<double>>& result) {
-    for (int row = 0; row < row_f_size_; ++row)
-      for (int col = 0; col < col_f_size_; ++col)
-        result[row][col] +=
-            in_1_[row][col_f_size_ - 1] * in_2_[row_f_size_ - 1][col];
+    for (int row = 0; row < row_one; ++row)
+      for (int col = 0; col < col_two; ++col)
+        result[row][col] += in_1_[row][row_two - 1] * in_2_[row_two - 1][col];
   }
 
+  // std::vector<std::vector<double>> Pipeline() {
+  //   std::vector<std::vector<double>> result_matrix(row_one);
+  //   for (auto& row : result_matrix) row.resize(col_two);
+
+  //   std::queue<double> row_q;
+  //   std::queue<double> mult_q;
+
+  //   bool t1_done = false;
+  //   bool t2_done = false;
+  //   bool t3_done = false;
+
+  //   auto col_factor = ColFactorParallel();
+
+  //   std::thread t1([&]() {
+  //     std::unique_lock<std::mutex> lock(gLock);
+
+  //     for (int row = 0; row < row_one; ++row) {
+  //       double result = 0;
+  //       for (int j = 0; j < half_; ++j) {
+  //         result += in_1_[row][2 * j] * in_1_[row][2 * j + 1];
+  //       }
+  //       row_q.emplace(result);
+  //       t1_done = true;
+  //       gConVar.notify_one();
+  //     }
+  //   });
+
+  //   int stop = row_one * col_two;
+
+  //   std::thread t2([&]() {
+  //     std::unique_lock<std::mutex> lock(gLock);
+  //     int c = 0, col = 0;
+  //     while (c != stop) {
+  //       while (!t1_done) {
+  //         gConVar.wait(lock);
+  //       }
+  //       mult_q.emplace(-row_q.front() - col_factor[col++]);
+  //       t2_done = true;
+  //       gConVar_2.notify_one();
+  //       c++;
+  //       if (c % col_two == 0) {
+  //         row_q.pop();
+  //         col = 0;
+  //       }
+  //     }
+  //   });
+
+  //   std::thread t3([&]() {
+  //     std::unique_lock<std::mutex> lock(gLock);
+  //     int c = 0, ro = 0, co = 0;
+  //     while (c != stop) {
+  //       while (!t2_done) {
+  //         gConVar_2.wait(lock);
+  //       }
+  //       result_matrix[ro][co] = mult_q.front();
+  //       for (auto k = 0; k < half_; ++k) {
+  //         result_matrix[ro][co] += (in_1_[ro][2 * k] + in_2_[2 * k + 1][co])
+  //         *
+  //                                  (in_1_[ro][2 * k + 1] + in_2_[2 * k][co]);
+  //       }
+  //       t3_done = true;
+  //       gConVar_3.notify_one();
+  //       co++;
+  //       if (co == col_two) {
+  //         co = 0;
+  //         ro++;
+  //       };
+  //       c++;
+  //       mult_q.pop();
+  //     }
+  //   });
+
+  //   std::thread t4([&]() {
+  //     std::unique_lock<std::mutex> lock(gLock);
+  //     int c = 0, ro = 0, co = 0;
+  //     if (even_) c = stop;
+  //     while (c != stop) {
+  //       while (!t3_done) {
+  //         gConVar_3.wait(lock);
+  //       }
+
+  //       result_matrix[ro][co] +=
+  //           in_1_[ro][row_two - 1] * in_2_[row_two - 1][co];
+  //       co++;
+
+  //       if (co == col_two) {
+  //         ro++;
+  //         co = 0;
+  //       }
+  //       c++;
+  //     }
+  //   });
+
+  //   if (t1.joinable()) t1.join();
+  //   if (t2.joinable()) t2.join();
+  //   if (t3.joinable()) t3.join();
+  //   if (t4.joinable()) t4.join();
+  //   return result_matrix;
+  // }
+
   std::vector<std::vector<double>> Pipeline() {
-    std::vector<std::vector<double>> result_matrix(row_f_size_);
-    for (auto& row : result_matrix) row.resize(col_f_size_);
+    std::vector<std::vector<double>> result_matrix(row_one);
+    for (auto& row : result_matrix) row.resize(col_two);
 
     std::queue<double> row_q;
-    // std::queue<double> col_q;
+    std::queue<double> mult_q;
 
-    auto col_factor = ColFactor();
-    std::cout << "PIPELINE" << std::endl;
+    bool t1_done = false;
+    bool t2_done = false;
+    bool t3_done = false;
+
+    auto col_factor = ColFactorParallel();
+
     std::thread t1([&]() {
-      for (int row = 0; row < row_f_size_; ++row) {
+      std::unique_lock<std::mutex> lock(gLock);
+
+      for (int row = 0; row < row_one; ++row) {
         double result = 0;
         for (int j = 0; j < half_; ++j) {
           result += in_1_[row][2 * j] * in_1_[row][2 * j + 1];
         }
         row_q.emplace(result);
+        t1_done = true;
+        gConVar.notify_one();
       }
     });
 
-    // std::thread t2([&]() {
-    //   for (int col = 0; col < col_f_size_; ++col) {
-    //     double result = 0;
-    //     for (int j = 0; j < half_; ++j) {
-    //       result += in_2_[2 * j][col] * in_2_[2 * j + 1][col];
-    //     }
-    //     col_q.emplace(result);
-    //   }
-    // });
+    int stop = row_one * col_two;
 
-    int stop = row_f_size_ * col_f_size_;
-
-    std::thread t3([&]() {
-      std::cout << "T3" << std::endl;
-      int c = 0, ro = 0;
+    std::thread t2([&]() {
+      std::unique_lock<std::mutex> lock(gLock);
+      int c = 0, col = 0;
       while (c != stop) {
-        if (!row_q.empty()) {
-          for (int col = 0; col < col_f_size_; ++col) {
-            result_matrix[ro][col] = -row_q.front() - col_factor[col];
-            c++;
-            for (auto k = 0; k < half_; ++k) {
-              result_matrix[ro][col] +=
-                  (in_1_[ro][2 * k] + in_2_[2 * k + 1][col]) *
-                  (in_1_[ro][2 * k + 1] + in_2_[2 * k][col]);
-            }
-          }
-
-          ro++;
+        while (!t1_done) {
+          gConVar.wait(lock);
+        }
+        mult_q.emplace(-row_q.front() - col_factor[col++]);
+        t2_done = true;
+        gConVar_2.notify_one();
+        c++;
+        if (c % col_two == 0) {
           row_q.pop();
-          //   col_q.pop();
-          std::cout << "c = " << c << std::endl;
+          col = 0;
         }
       }
     });
 
-    t1.join();
-    std::cout << "T1 done" << std::endl;
-    // t2.join();
-    std::cout << "t2 done" << std::endl;
-    t3.join();
+    std::thread t3([&]() {
+      std::unique_lock<std::mutex> lock(gLock);
+      int c = 0, ro = 0, co = 0;
+      while (c != stop) {
+        while (!t2_done) {
+          gConVar_2.wait(lock);
+        }
+        result_matrix[ro][co] = mult_q.front();
+        for (auto k = 0; k < half_; ++k) {
+          result_matrix[ro][co] += (in_1_[ro][2 * k] + in_2_[2 * k + 1][co]) *
+                                   (in_1_[ro][2 * k + 1] + in_2_[2 * k][co]);
+        }
+        t3_done = true;
+        gConVar_3.notify_one();
+        co++;
+        if (co == col_two) {
+          co = 0;
+          ro++;
+        };
+        c++;
+        mult_q.pop();
+      }
+    });
 
+    std::thread t4([&]() {
+      std::unique_lock<std::mutex> lock(gLock);
+      int c = 0, ro = 0, co = 0;
+      if (even_) c = stop;
+      while (c != stop) {
+        while (!t3_done) {
+          gConVar_3.wait(lock);
+        }
+
+        result_matrix[ro][co] +=
+            in_1_[ro][row_two - 1] * in_2_[row_two - 1][co];
+        co++;
+
+        if (co == col_two) {
+          ro++;
+          co = 0;
+        }
+        c++;
+      }
+    });
+
+    if (t1.joinable()) t1.join();
+    if (t2.joinable()) t2.join();
+    if (t3.joinable()) t3.join();
+    if (t4.joinable()) t4.join();
     return result_matrix;
   }
 
  private:
   std::vector<std::vector<double>> in_1_;
   std::vector<std::vector<double>> in_2_;
-  int row_f_size_ = 0;
-  int col_f_size_ = 0;
+  int row_one = 0;
+  int col_one = 0;
+  int row_two = 0;
+  int col_two = 0;
   int half_ = 0;
+  bool even_ = false;
 
-  //   std::queue<double> row_q;
-  //   std::queue<double> col_q;
+  std::mutex gLock;
+  std::condition_variable gConVar;
+  std::condition_variable gConVar_2;
+  std::condition_variable gConVar_3;
 };
 
 #endif  //  SRC_MODEL_WINOGRAD_H_
